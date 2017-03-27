8c5555eb6b058e05f7e6e272d321d3886f43650a
Kevin Mannix <kmannix@bu.edu>
2017-03-26 20:52:09 -0400

    init elixir project

---
+++ lib/oddsshark.ex
@@ -0,0 +1,18 @@
+defmodule OddsShark do
+  @moduledoc """
+  Documentation for OddsShark.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> OddsShark.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/oddsshark_test.exs
@@ -0,0 +1,8 @@
+defmodule OddsSharkTest do
+  use ExUnit.Case
+  doctest OddsShark
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
